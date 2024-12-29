3234f2957576468109eed00ecc603c19b29c3754
charanm927 <charanm927@gmail.com>
2024-12-29 08:45:52 +0530

    chore: update revalidateQuote

--- src/payload/hooks/revalidateQuotes.ts
+++ src/payload/hooks/revalidateQuotes.ts
@@ -7,8 +7,6 @@ export const revalidateQuotes: CollectionAfterChangeHook<Quote> = async ({
 }) => {
   // if page is published & their is no dynamic block directly revalidating the page
   if (doc._status === 'published') {
-    revalidateTag('list-tags-with-blog-count')
-
     revalidateTag('list-quotes')
     revalidateTag(`details-quotes-${doc?.slug}`)
     console.log(`list-quotes at ${new Date().getTime()}`)
