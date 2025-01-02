20c66acb7ca8f6470210c1eb361f6bee52f3846d
charanm927 <charanm927@gmail.com>
2025-01-02 12:23:09 +0530

    chore: change import paths

--- src/payload/blocks/blockConfig.ts
+++ src/payload/blocks/blockConfig.ts
@@ -1,9 +1,9 @@
 // This is just to consolidate all the existing blocks and their respective JSX components
 // Always prefer to individually import the required block or JSX in other parts of your application
 // Importing the block components and its configurations
-import { DetailsConfig } from './Details'
-import { FormConfig } from './Form'
-import { ListConfig } from './List'
+import DetailsConfig from './Details/config'
+import FormConfig from './Form/config'
+import ListConfig from './List/config'
 
 // Exporting an object that maps block names (as keys) to their corresponding JSX components (as values)
 // This object allows dynamic rendering of components based on the block names
