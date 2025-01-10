8a0c3039946ab9620fa75abc56e8b19b0678a577
charanm927 <charanm927@gmail.com>
2025-01-10 11:37:30 +0530

    chore: upgraded to latest changes

--- payload.config.ts
+++ payload.config.ts
@@ -20,6 +20,17 @@ import { revalidateTags } from '@/payload/hooks/revalidateTags'
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
+const convertRailwayURL = (url: string) => {
+  const railwayDomain = '.up.railway.app'
+  const contentqlDomain = '.contentql.io'
+  // Check if the URL ends with .up.railway.app or contains it
+  if (url.includes(railwayDomain)) {
+    return url.replace(railwayDomain, contentqlDomain)
+  }
+  // Return the original URL if it doesn't match
+  return url
+}
+
 export default cqlConfig({
   admin: {
     components: {
@@ -29,8 +40,8 @@ export default cqlConfig({
       },
     },
   },
-  cors: [env.PAYLOAD_URL],
-  csrf: [env.PAYLOAD_URL],
+  cors: [env.PAYLOAD_URL, convertRailwayURL(env.PAYLOAD_URL)],
+  csrf: [env.PAYLOAD_URL, convertRailwayURL(env.PAYLOAD_URL)],
 
   baseURL: env.PAYLOAD_URL,
 
